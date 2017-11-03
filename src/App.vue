a1a42b8577e72756f0447d4759f82ced32bf5904
IsmaelJdz <ismael_br7@hotmail.com>
2017-11-03 00:30:21 -0600

    App Spa con vueJs consumiento la API de lastfm para los artistas del momento de cada pais terminada

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,69 @@
 <template lang="pug">
-  #app
+#app
     img(src='./assets/logo.png')
     h1 PlatziMusic
+    select(v-model="selectedCountry")
+        option(v-for="country in countries" v-bind:value="country.value") {{ country.name }}
+    spinner(v-show="loading")
     ul
-      li(v-for="artist in artists") {{artist.name}}
-              
+        artist(v-for="artist in artists" v-bind:artist="artist" v-bind:key="artist.mbid")
 </template>
 
 <script>
 
+import Artist from './components/Artist.vue'
+import Spinner from './components/Spinner.vue'
 import getArtists from './api'
 
 export default {
   name: 'app',
   data () {
     return {
-      artists: []
+      artists: [],
+      countries: [
+          { name: 'Argentina', value: 'argentina' },
+          { name: 'Colombia', value: 'colombia' },
+          { name: 'Mexico', value: 'mexico' }
+      ],
+      selectedCountry: 'argentina',
+      loading: true
+    }
+  },
+  components: {
+    Artist: Artist,
+    Spinner: Spinner
+  },
+  methods: {
+    refreshArtists() {
+        const self = this
+        this.loading = true
+        this.artists = []
+        getArtists(this.selectedCountry)
+          .then(function (artists){
+            self.loading = false
+            self.artists = artists
+        })
     }
   },
   mounted: function () {
-      const self = this
-      getArtists()
-        .then(function (artists){
-          self.artists = artists
-      })
+      this.refreshArtists()
+  },
+  watch: {
+      selectedCountry: function () {
+          this.refreshArtists()
+      }
   }
 }
 </script>
 
-<style lang="stylus">
-  #app
-    font-family 'Avenir', Helvetica, Arial, sans-serif
-    -webkit-font-smoothing antialiased
-    -moz-osx-font-smoothing grayscale
-    text-align center
-    color red !important
-    margin-top 60px
+<style lang="stylus" scoped>
+    #app
+        font-family 'Avenir', Helvetica, Arial, sans-serif
+        -webkit-font-smoothing antialiased
+        -moz-osx-font-smoothing grayscale
+        text-align center
+        color red !important
+        margin-top 60px
 
 h1, h2
     font-weight normal
